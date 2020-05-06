2c7ad1da1cfb9b64ee36c48ca17da39d6301a42c
Ryan-Red <45923095+Ryan-Red@users.noreply.github.com>
2020-05-05 21:10:22 -0400

    Did stuff

--- NeuralNet/main.py
+++ NeuralNet/main.py
@@ -1,2 +1,28 @@
 import numpy as np
+import mnist
+
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = np.random.randn((n_inputs,n_neurons))
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        return np.dot(inputs, self.weights) + self.biases
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+#normalize pixel values from [0,255] to [-0.5,0.5]
+
+train_images = (train_images/255) - 0.5
+test_images = (test_images/255) - 0.5
+
+#Flatten the Images. Flatten the 28x28 images into a 784 vector
+
+train_images = train_images.reshape((-1,784))
+test_images = test_images.reshape((-1,784))
+
+
 
